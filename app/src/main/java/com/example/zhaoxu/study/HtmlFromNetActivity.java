197627662bd118d82aab2a0b6cc0576f1133b3a0
Administrator <zhaoxukl1314@163.com>
2016-05-19 08:23:15 +0800

    Add post

--- app/src/main/java/com/example/zhaoxu/study/HtmlFromNetActivity.java
+++ app/src/main/java/com/example/zhaoxu/study/HtmlFromNetActivity.java
@@ -3,8 +3,10 @@ package com.example.zhaoxu.study;
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,10 +17,11 @@ import com.example.zhaoxu.study.netUtils.HtmlNetUtils;
  */
 public class HtmlFromNetActivity extends Activity {
 
+    private static final String TAG = "HtmlFromNetActivity";
     private Button btn_get;
     private Button btn_post;
-    private TextView tv_username;
-    private TextView tv_password;
+    private EditText tv_username;
+    private EditText tv_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +29,10 @@ public class HtmlFromNetActivity extends Activity {
         setContentView(R.layout.html_from_net);
         btn_get = (Button) findViewById(R.id.html_get);
         btn_post = (Button) findViewById(R.id.html_post);
-        tv_username = (TextView) findViewById(R.id.html_username);
-        tv_password = (TextView) findViewById(R.id.html_password);
+        tv_username = (EditText) findViewById(R.id.html_username);
+        tv_password = (EditText) findViewById(R.id.html_password);
         btn_get.setOnClickListener(new getOnclickListner());
+        btn_post.setOnClickListener(new postOnclickListner());
     }
 
     private class getOnclickListner implements View.OnClickListener {
@@ -47,10 +51,43 @@ public class HtmlFromNetActivity extends Activity {
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
+                                Log.e(TAG,"run");
                                 if (TextUtils.isEmpty(html)) {
-                                    Toast.makeText(HtmlFromNetActivity.this,"连接失败",Toast.LENGTH_LONG);
+                                    Toast.makeText(HtmlFromNetActivity.this,"连接失败",Toast.LENGTH_LONG).show();
                                 } else {
-                                    Toast.makeText(HtmlFromNetActivity.this,html,Toast.LENGTH_LONG);
+                                    Log.e(TAG,"该显示toast了");
+                                    Toast.makeText(HtmlFromNetActivity.this,html,Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                    }
+                }).start();
+            }
+        }
+    }
+
+    private class postOnclickListner implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            final String userName = tv_username.getText().toString();
+            final String password = tv_password.getText().toString();
+            if (TextUtils.isEmpty(userName) && TextUtils.isEmpty(password)) {
+                Toast.makeText(HtmlFromNetActivity.this,"账号密码不能为空",Toast.LENGTH_LONG);
+            } else {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        final String html = HtmlNetUtils.postStringFromNet(userName,password);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Log.e(TAG,"run");
+                                if (TextUtils.isEmpty(html)) {
+                                    Toast.makeText(HtmlFromNetActivity.this,"连接失败",Toast.LENGTH_LONG).show();
+                                } else {
+                                    Log.e(TAG,"该显示toast了");
+                                    Toast.makeText(HtmlFromNetActivity.this,html,Toast.LENGTH_LONG).show();
                                 }
                             }
                         });
--- app/src/main/java/com/example/zhaoxu/study/netUtils/HtmlNetUtils.java
+++ app/src/main/java/com/example/zhaoxu/study/netUtils/HtmlNetUtils.java
@@ -1,27 +1,35 @@
 package com.example.zhaoxu.study.netUtils;
 
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
+import java.net.URLEncoder;
 
 /**
  * Created by Administrator on 2016/5/18.
  */
 public class HtmlNetUtils {
 
+    private static final String TAG = "HtmlNetUtils";
+    private static HttpURLConnection httpURLConnection;
+
     public static String getStringFromNet(String userName, String password) {
-        String url = "http://10.0.0.2:8080/ServerItheima28/servlet/LoginServlet?username=" + userName + "&password=" + password;
+        String url = "http://10.0.2.2:8080/ServerItheima28/servlet/LoginServlet?username=" + URLEncoder.encode(userName) + "&password=" + URLEncoder.encode(password);
         try {
             URL murl = new URL(url);
-            HttpURLConnection httpURLConnection = (HttpURLConnection) murl.openConnection();
+            httpURLConnection = (HttpURLConnection) murl.openConnection();
 
             httpURLConnection.setRequestMethod("GET");
             httpURLConnection.setReadTimeout(10000);
             httpURLConnection.setConnectTimeout(5000);
+            httpURLConnection.connect();
             int requestCode = httpURLConnection.getResponseCode();
             if (requestCode == 200){
                 InputStream inputStream = httpURLConnection.getInputStream();
@@ -34,6 +42,7 @@ public class HtmlNetUtils {
                 String html = byteArray.toString();
                 inputStream.close();
                 byteArray.close();
+                Log.e(TAG,html);
                 return html;
             } else {
                 return null;
@@ -43,6 +52,56 @@ public class HtmlNetUtils {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if(httpURLConnection != null) {
+                httpURLConnection.disconnect();
+            }
+        }
+        return null;
+    }
+
+    public static String postStringFromNet(String userName, String password) {
+        String url = "http://10.0.2.2:8080/ServerItheima28/servlet/LoginServlet";
+        String data = "username=" + userName + "&password=" + password;
+        try {
+            URL murl = new URL(url);
+            httpURLConnection = (HttpURLConnection) murl.openConnection();
+
+            httpURLConnection.setRequestMethod("POST");
+            httpURLConnection.setReadTimeout(10000);
+            httpURLConnection.setConnectTimeout(5000);
+            httpURLConnection.setDoOutput(true);
+            OutputStream outputStream = httpURLConnection.getOutputStream();
+            outputStream.write(data.getBytes());
+            outputStream.flush();
+            outputStream.close();
+
+            int requestCode = httpURLConnection.getResponseCode();
+            if (requestCode == 200){
+                InputStream inputStream = httpURLConnection.getInputStream();
+                byte[] buffer = new byte[1024];
+                ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+                int len = -1;
+                while ((len = inputStream.read(buffer)) != -1) {
+                    byteArray.write(buffer,0,len);
+                }
+                String html = byteArray.toString();
+                inputStream.close();
+                byteArray.close();
+                Log.e(TAG,html);
+                return html;
+            } else {
+                return null;
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(httpURLConnection != null) {
+                httpURLConnection.disconnect();
+            }
         }
         return null;
     }

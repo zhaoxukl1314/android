054695926d59bbce09d45f21e437a8b630c62869
Administrator <zhaoxukl1314@163.com>
2016-06-14 08:38:43 +0800

    test push

---
+++ app/src/main/java/com/example/zhaoxu/study/Activity/MusicPlayerActivity.java
@@ -0,0 +1,9 @@
+package com.example.zhaoxu.study.Activity;
+
+import android.app.Activity;
+
+/**
+ * Created by Administrator on 2016/6/14.
+ */
+public class MusicPlayerActivity extends Activity {
+}
